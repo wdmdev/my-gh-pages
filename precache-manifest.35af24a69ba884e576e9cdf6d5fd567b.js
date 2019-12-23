ad6a1fe4fdf0897290c940edfe40f95decaeb27c
William Marstrand <william.marstrand@gmail.com>
2019-12-23 16:09:42 +0100

    Updates

---
+++ precache-manifest.35af24a69ba884e576e9cdf6d5fd567b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9641cd75c297ebc9ce6c808deaefb850",
+    "url": "/my-gh-pages/index.html"
+  },
+  {
+    "revision": "a602447a7193110d23ed",
+    "url": "/my-gh-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "88a06a484f86af5fd403",
+    "url": "/my-gh-pages/static/js/2.91b558e5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-gh-pages/static/js/2.91b558e5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a602447a7193110d23ed",
+    "url": "/my-gh-pages/static/js/main.fc4c634d.chunk.js"
+  },
+  {
+    "revision": "78f941bb9c3f0d9ee967",
+    "url": "/my-gh-pages/static/js/runtime-main.57a19f0c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
