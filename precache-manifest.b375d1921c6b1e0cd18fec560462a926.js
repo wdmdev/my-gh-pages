8d6c906e3a3f50cfabd295d8c3dbfe388c5f7338
William Marstrand <william.marstrand@gmail.com>
2020-02-01 12:28:21 +0100

    Updates

---
+++ precache-manifest.b375d1921c6b1e0cd18fec560462a926.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bacc2d3b1f4dbf002a5df90a05f2dfe7",
+    "url": "/my-gh-pages/index.html"
+  },
+  {
+    "revision": "1a05dc9de8c121705d85",
+    "url": "/my-gh-pages/static/css/main.a83db560.chunk.css"
+  },
+  {
+    "revision": "5de627d4f9cd7d964270",
+    "url": "/my-gh-pages/static/js/2.a1159736.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/my-gh-pages/static/js/2.a1159736.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1a05dc9de8c121705d85",
+    "url": "/my-gh-pages/static/js/main.e8d1321b.chunk.js"
+  },
+  {
+    "revision": "43975dce58a3a13965a7",
+    "url": "/my-gh-pages/static/js/runtime-main.559bde0e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-gh-pages/precache-manifest.6647d5a20f0065eef4da4f7cc9982036.js"
+  "/my-gh-pages/precache-manifest.b375d1921c6b1e0cd18fec560462a926.js"
 );
 
 self.addEventListener('message', (event) => {
